9d5abab6a126cb894276187a7a743f85ab2c4d01
Arisha-Awan <arishaawan1999@gmail.com>
2024-10-19 21:07:40 +0500

    binary number finding

--- BinaryNumber.py
+++ BinaryNumber.py
@@ -1,3 +1,4 @@
+# If we want to return binary number string
 def dec_to_Binary(num:int):
     bin_num=""
     while num>0:
@@ -5,6 +6,19 @@ def dec_to_Binary(num:int):
         bin_num=str(rem)+bin_num
         num=num//2
     return bin_num
+# if we want to return binary number
+def dec_to_binary(num: int) -> int:
+    binary_num = 0  # Store the binary number as an integer
+    place_value = 1  # Track the place value (1, 10, 100, etc.)
+
+    while num > 0:
+        rem = num % 2  # Get the remainder (0 or 1)
+        binary_num += rem * place_value  # Add the binary digit at the correct place value
+        place_value *= 10  # Move to the next place value
+        num = num // 2  # Use integer division to update the number
+
+    return binary_num
+
 
 
 num=9
