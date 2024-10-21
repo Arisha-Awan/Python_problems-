ad9f5b222b31196990984413c9d4cf5eb8909693
Arisha-Awan <arishaawan1999@gmail.com>
2024-10-21 07:48:15 +0500

    find a number if it is power of 2 or not

--- BinaryNumber.py
+++ BinaryNumber.py
@@ -48,3 +48,27 @@ print(f"bitwise right-shift 10 by 2 :{10>>2}")
 # Shift right by pushing copies of the leftmost bit in from the left, and let
 # the rightmost bits fall off
 # Formula:   a>>b==a//2^b
+
+
+# Find a number if it is power of 2 or not
+def is_power_of_2(n):
+    return n > 0 and (n & (n - 1)) == 0
+
+# Test cases
+num=16
+print(f"{num} is power of 2 {is_power_of_2(num)}")  # True
+num=18
+print(f"{num} is power of 2 {is_power_of_2(num)}")  # False
+
+# 1-For n = 16 (binary: 10000):
+# =>n−1=15 (binary: 01111)
+# =>10000 & 01111 = 00000 (True)
+#
+# 2-For n = 18 (binary: 10010):
+# =>n−1=17 (binary: 10001)
+# =>10010 & 10001 = 10000 (False)
+# This method is efficient and checks if a number is a power of 2 in constant time 𝑂(1).
+# ==>Explanation
+#  n&(n−1)=0 is true, where & is the bitwise AND operator. This works because subtracting
+#  1 from a power of 2 flips all bits after the rightmost set bit to 1, and that bit itself to 0.
+#  Taking the AND of 𝑛 n and 𝑛 − 1 n−1 will yield 0 if 𝑛 n is a power of 2.
--- reverse_number.py
+++ reverse_number.py
@@ -17,24 +17,3 @@ def reverse_num_str(num:int)->int:
 num=5881
 print(f"reverse of {num} is: {reverse_number(num)}")
 
-def is_power_of_2(n):
-    return n > 0 and (n & (n - 1)) == 0
-
-# Test cases
-num=16
-print(f"{num} is power of 2 {is_power_of_2(num)}")  # True
-num=18
-print(f"{num} is power of 2 {is_power_of_2(num)}")  # False
-
-# 1-For n = 16 (binary: 10000):
-# =>n−1=15 (binary: 01111)
-# =>10000 & 01111 = 00000 (True)
-#
-# 2-For n = 18 (binary: 10010):
-# =>n−1=17 (binary: 10001)
-# =>10010 & 10001 = 10000 (False)
-# This method is efficient and checks if a number is a power of 2 in constant time 𝑂(1).
-# ==>Explanation
-#  n&(n−1)=0 is true, where & is the bitwise AND operator. This works because subtracting
-#  1 from a power of 2 flips all bits after the rightmost set bit to 1, and that bit itself to 0.
-#  Taking the AND of 𝑛 n and 𝑛 − 1 n−1 will yield 0 if 𝑛 n is a power of 2.
